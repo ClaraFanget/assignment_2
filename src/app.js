04e23557be2c31b53b43d525e42f010a1b57e60b
Clara <cfanget@ensc.fr>
2025-12-14 22:25:41 +0900

    [UPDATE] - Delete health

--- src/app.js
+++ src/app.js
@@ -33,7 +33,6 @@ router.use("/orders", require("./routes/order.route.js"));
 router.use("/", require("./routes/review.route.js"));
 router.use("/", require("./routes/comment.route.js"));
 router.use("/coupons", require("./routes/coupon.route.js"));
-router.use("/health", require("./routes/health.route.js"));
 
 app.get("/", (req, res) => {
   res.send("Welcome on the bookstore API");
