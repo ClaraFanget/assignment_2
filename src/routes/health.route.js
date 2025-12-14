7ca62446f6bf4293e4d1e3d4ca38b15aa53d7cac
Clara <cfanget@ensc.fr>
2025-12-14 22:21:36 +0900

    [UPDATE] - Fix health

--- src/routes/health.route.js
+++ src/routes/health.route.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 
-const packageJson = require("../package.json");
+const packageJson = require("../../package.json");
 
 /**
  * @swagger
